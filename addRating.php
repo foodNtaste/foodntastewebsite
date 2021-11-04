afbe7689879e8a7cd1982c09dc5c08c346dddac2
Not a Stranger to Esport <thisisnotestguys@gmail.com>
2021-11-04 18:50:21 +0100

    added: addRating.php

---
+++ addRating.php
@@ -0,0 +1,33 @@
+<?php
+    $host_name = "localhost";
+    $database = "***";          // Change your database name
+    $username = "***";          // Your database user id
+    $password = "***";          // Your password
+
+    //error_reporting(0);// With this no error reporting will be there
+    //////// Do not Edit below /////////
+
+    $connection = mysqli_connect($host_name, $username, $password, $database);
+
+    if (!$connection) {
+        echo "Error: Unable to connect to MySQL.<br>";
+        echo "<br>Debugging errno: " . mysqli_connect_errno();
+        echo "<br>Debugging error: " . mysqli_connect_error();
+        exit;
+    }
+
+    // get data from post request
+    $rating = $_POST['rating'];
+    // insert rating into database
+    // get current date
+    $date = date('Y-m-d');
+    // get current time
+    $time = date('H:i:s');
+    $query = "INSERT INTO bewertungen (essen, bewertung, datum, zeit) VALUES ('m1', '$rating', '$date', '$time')";
+    $result = mysqli_query($connection, $query);
+    if (!$result) {
+        die("Database query failed.");
+    }
+    // close connection
+    mysqli_close($connection);
+?>
