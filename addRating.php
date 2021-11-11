bf6750024402301cf1958d14aa058fa9f76f3cff
Luiggi33 <aron.rohr@web.de>
2021-11-11 19:14:04 +0100

    add: GET Parameter essen

--- addRating.php
+++ addRating.php
@@ -18,10 +18,17 @@
 
     // get data from GET request
     $rating = $_GET['rating'];
+    $essen = $_GET['essen'];
     // check if rating is not gut, mittel or schlecht
     if ($rating != "gut" && $rating != "mittel" && $rating != "schlecht") {
         echo "<h1>Error: Invalid rating</h1>";
         echo "<h2>Argumente für rating sind gut, mittel oder schlecht</h2>";
+        exit;
+    }
+    // check if essen is m1 m2 or m3
+    if ($essen != "m1" && $essen != "m2" && $essen != "m3") {
+        echo "<h1>Error: Invalid essen</h1>";
+        echo "<h2>Argumente für essen sind m1, m2 oder m3</h2>";
         exit;
     }
     // insert rating into database
@@ -29,7 +36,7 @@
     $date = date('Y-m-d');
     // get current time
     $time = date('H:i:s');
-    $query = "INSERT INTO bewertungen (essen, bewertung, datum, zeit) VALUES ('m1', '$rating', '$date', '$time')";
+    $query = "INSERT INTO bewertungen (essen, bewertung, datum, zeit) VALUES ('$essen', '$rating', '$date', '$time')";
     $result = mysqli_query($connection, $query);
     if (!$result) {
         die("Database query failed.");
--- getData.php
+++ getData.php
@@ -10,17 +10,17 @@
   $connection = mysqli_connect($host_name, $username, $password, $database);
 
   if (!$connection) {
-      echo "Error: Unable to connect to MySQL.<br>";
-      echo "<br>Debugging errno: " . mysqli_connect_errno();
-      echo "<br>Debugging error: " . mysqli_connect_error();
-      exit;
+    echo "Error: Unable to connect to MySQL.<br>";
+    echo "<br>Debugging errno: " . mysqli_connect_errno();
+    echo "<br>Debugging error: " . mysqli_connect_error();
+    exit;
   }
   $sql = "select * from bewertungen";
   $result = mysqli_query($connection, $sql) or die("Error in Selecting " . mysqli_error($connection));
   $emparray = array();
   while($row =mysqli_fetch_assoc($result))
   {
-      $emparray[] = $row;
+    $emparray[] = $row;
   }
   echo json_encode($emparray);
 ?>
